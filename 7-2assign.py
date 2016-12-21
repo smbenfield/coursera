452cd3a987982283244fe3979cb26f896fe006a5
Shelby Benfield <benfield.shelby@gmail.com>
2016-12-20 20:43:28 -0500

    Final copy.

--- 7-2assign.py
+++ 7-2assign.py
@@ -1,10 +1,15 @@
 # Use the file name mbox-short.txt as the file name
 fname = raw_input("Enter file name: ")
 fh = open(fname)
+x = 0
+numtotal = 0
 for line in fh:
     if line.startswith("X-DSPAM-Confidence:"):
+        x = x + 1
         numline = line[20:26]
-        print numline
-        floatline = numline.float()
-        print floatline
-print "Done"
+        numfloat = float(numline)
+        numtotal = numtotal + numfloat
+    else:
+    	continue
+avg = numtotal/x
+print "Average spam confidence: ",avg
