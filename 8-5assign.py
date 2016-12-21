1d2bff3ebad2710a2063fd290055d09f77586af4
Shelby Benfield <benfield.shelby@gmail.com>
2016-12-21 18:33:50 -0500

    Fixed Assignment 8.5

--- 8-5assign.py
+++ 8-5assign.py
@@ -1,21 +1,17 @@
 count = 0
 numbr = 0
-test = 0
 email = []
-delimiter = "/n"
 
 fname = raw_input("Enter file name: ")
 fname = "mbox-short.txt"
 fh = open(fname)
 
 for line in fh:
-	test += 1
 	lsplt = line.rstrip()
 	if not line.startswith("From "): continue
 	lword = lsplt.split(" ")
 	email.append(lword[1])
+	print lword[1]
 	count += 1
-	numbr +=1
-print test
-print email
+	lengl = len(email)
 print "There were", count, "lines in the file with From as the first word"
