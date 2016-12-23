c6add3f3e8bcad63460d1004aa5f2c3bd138e161
Shelby Benfield <benfield.shelby@gmail.com>
2016-12-23 13:56:43 -0500

    Begun course 3. Notes added.

--- course3/ch11notes.py
+++ course3/ch11notes.py
@@ -2,9 +2,11 @@
 import re
 
 # Searches for a matching string to a reg ex
+# Returns True/False as to whether that string contains the thing
 # re.search()
 
 # Searches for portions of a string to match a reg ex
+# Returns python list of instances of said thing
 # re.findall()
 
 # Searches for lines with From:
@@ -13,3 +15,11 @@ for line in hand:
     line = line.rstrip()
     if re.search('^From:', line): # ^ means starts with
         print line
+
+
+# Starts with (^) "X" Any character (.) Zero or more times (*) :
+"^X.*:"
+# Starts with (^) "X-" Any non-whitespace character (\S) One or more times (+) :
+"^X-\S+:"
+# One digit inside brackets, then one or more digits (+)
+"[0-9]+"
