7b2599095186c6ff4b6b1617abd98cbf21971a36
Shelby Benfield <benfield.shelby@gmail.com>
2016-12-23 13:57:00 -0500

    Begun course 3.

--- course3/ch11notes.py
+++ course3/ch11notes.py
@@ -16,6 +16,24 @@ for line in hand:
     if re.search('^From:', line): # ^ means starts with
         print line
 
+# Repeat characters (* +) return as large of a string as possible - greedy
+# (?) Does not do greedy matching
+# \$ = Real $ sign
+
+# ^	           Matches the beginning of a line
+# $	           Matches the end of the line
+# .	           Matches any character
+# \s	       Matches whitespace
+# \S	       Matches any non-whitespace character
+# *	           Repeats a character zero or more times
+# *?           Repeats a character zero or more times (non-greedy)
+# +            Repeats a character one or mormae times
+# +?           Repeats a character one or more times (non-greedy)
+# [aeiou]      Matches a single character in the listed set
+# [^XYZ]	   Matches a single character not in the listed set
+# [a-z0-9]     The set of characters can include a range
+# (	           Indicates where string extraction is to start
+# )	           Indicates where string extraction is to end
 
 # Starts with (^) "X" Any character (.) Zero or more times (*) :
 "^X.*:"
@@ -23,3 +41,13 @@ for line in hand:
 "^X-\S+:"
 # One digit inside brackets, then one or more digits (+)
 "[0-9]+"
+# Looking for 1 or more uppercased vowel
+"[AEIOU]+"
+# Looking for email address formatting
+"\S+@\S+"
+# Begins with "From", extract email address
+"^From (\S+@\S+)"
+# Find an @ sign, extract until next single character is a space
+"@([^ ]*)"
+# Look for From and any number of characters until @, then extract until space
+"^From .*@([^ ]*)"
