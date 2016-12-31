9c3b6a751fc8ab155094a637678f4979657fb255
Shelby Benfield <benfield.shelby@gmail.com>
2016-12-31 18:11:02 -0500

    Began many-to-many project.

---
+++ course4/ch15assign.py
@@ -0,0 +1,63 @@
+import sqlite3
+import json
+import xml.etree.ElementTree as ET
+
+conn = sqlite3.connect('coursedb.sqlite')
+cur = conn.cursor()
+
+cur.executescript('''
+drop table if exists User;
+drop table if exists Course;
+drop table if exists Member;
+''')
+
+cur.executescript('''
+
+    CREATE TABLE User(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        name TEXT,
+        email TEXT
+    );
+
+    CREATE TABLE Course(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        title TEXT
+    );
+
+    CREATE TABLE Member(
+        user_id INTEGER,
+        course_id INTEGER,
+            role    INTEGER,
+        PRIMARY KEY (user_id, course_id)
+    )''')
+
+cur.executescript('''
+
+    INSERT INTO User (name, email) VALUES('Jane', 'jane@tsugi.org');
+    INSERT INTO User (name, email) VALUES('Ed', 'ed@tsugi.org');
+    INSERT INTO User (name, email) VALUES('Sue', 'sue@tsugi.org');
+    INSERT INTO Course (title) VALUES ('Python');
+    INSERT INTO Course (title) VALUES ('SQL');
+    INSERT INTO Course (title) VALUES ('PHP')
+    ''')
+
+cur.executescript('''
+    INSERT INTO Member (user_id, course_id, role) VALUES (1,1,1);
+    INSERT INTO Member (user_id, course_id, role) VALUES (2,1,0);
+    INSERT INTO Member (user_id, course_id, role) VALUES (3,1,0);
+
+    INSERT INTO Member (user_id, course_id, role) VALUES (1,2,0);
+    INSERT INTO Member (user_id, course_id, role) VALUES (2,2,1);
+
+    INSERT INTO Member (user_id, course_id, role) VALUES (2,3,1);
+    INSERT INTO Member (user_id, course_id, role) VALUES (3,3,0);
+    ''')
+
+cur.executescript('''
+    SELECT User.name, Member.role, Course.title
+    FROM User JOIN Member JOIN Course
+    ON Member.user_id = User.id AND Member.course_id = Course.id
+    ORDER BY Course.title, Member.role DESC, User.name
+    ''')
+
+conn.commit()
